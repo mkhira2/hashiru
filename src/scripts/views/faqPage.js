00834d07a8878c274b116e48118b36733a8f552f
Kenji Hirabayashi <mkhira2@gmail.com>
2017-04-02 15:59:05 -0500

    filled faq page with dummy data

--- src/scripts/views/faqPage.js
+++ src/scripts/views/faqPage.js
@@ -8,8 +8,16 @@ var FAQPage = React.createClass({
 			<div className='faqPage'>
 				<Banner />
 				<br /><br />
-				<h2>What the heck does Hashiru mean, anyway?</h2>
-				<h4>Hashiru is the Japanese word for "run"</h4>
+				<div className='faqs'>
+					<h2>What the heck does Hashiru mean, anyway?</h2>
+					<h4>Hashiru is the Japanese word for "run"</h4>
+					<h2>So, why should I Hashiru?</h2>
+					<h4>Running has many benefits for the mind and body.</h4>
+					<h2>How do I use your site?</h2>
+					<h4>It's easy! Just make an account. After you run, come back here and log your miles.</h4>
+					<h2>Why?</h2>
+					<h4>So you can earn online badges that aren't worth anything, of course.</h4>
+				</div>
 			</div>
 		)
 	}
