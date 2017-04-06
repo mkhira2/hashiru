7f8377a1ab241f46c81f69a79690306d4e51498c
Kenji Hirabayashi <mkhira2@gmail.com>
2017-04-06 18:42:33 -0500

    progress bar showing on every page

--- src/scripts/components/userInfo.js
+++ src/scripts/components/userInfo.js
@@ -6,10 +6,21 @@ import STORE from '../store'
 
 var UserInfo = React.createClass({
 	render: function() {
+
+		var updateProgressBar = {
+		height: '11px',
+		background: 'blue',
+		textAlign: 'left',
+		margin: 0,
+		padding: 0,
+		width: User.getCurrentUser().get('expPoints') % 10 + '0%'
+	}
+	
 		return (
 			<div className='userInfo'>
 				<h2 className='nameUserInfo'>{ACTIONS.checkLogInName()}</h2>
 				<h2 className='levelUserInfo'>{ACTIONS.checkLevel()}</h2>
+				<div className='progressBar' style={updateProgressBar}></div>
 			</div>
 		)
 	}
